2e14eb053336106e024f5f1e3dc6474506ab19d3
Juan Nieblas <juanpablo.n@rivka.mx>
2020-10-25 15:13:23 -0700

    first commit

---
+++ SignalsFinal/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SignalsFinal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button1_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (Button1.Content.ToString() == "Empezar")
+            {
+                Button1.Content = "Detener";
+                Button1.Background = Brushes.DarkRed;
+                Application.Current.MainWindow.Height = 800;
+                STT stt = new STT();
+                await stt.Main();
+
+            }
+            else
+            {
+                Button1.Content = "Empezar";
+                Button1.Background = Brushes.DarkGreen;
+                Application.Current.MainWindow.Height = 300;
+            }
+        }
+    }
+}
---
+++ SignalsFinal/STT.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.CognitiveServices.Speech;
+
+namespace SignalsFinal
+{
+    public class STT
+    {
+        static readonly string SPEECH_SERVICE_KEY = Environment.GetEnvironmentVariable(nameof(SPEECH_SERVICE_KEY));
+        static readonly string SPEECH_SERVICE_REGION = Environment.GetEnvironmentVariable(nameof(SPEECH_SERVICE_REGION));
+        public async Task Main()
+        {
+            await RecognizeSpeechAsync();
+        }
+        static async Task RecognizeSpeechAsync()
+        {
+            var config = SpeechConfig.FromSubscription("asd", "asd");
+            using var recognizer = new SpeechRecognizer(config);
+            MessageBox.Show("El programa comenzara a escuchar tu equipo");
+            var result = await recognizer.RecognizeOnceAsync();
+            var reason = GetRecognitionResultReason(result);
+            MessageBox.Show(reason)
+            MainWindow mw = new MainWindow();
+            mw.Signal.Source = System.Windows.Media.ImageSource(BitmapSource("Images/Image1.jpg"));
+
+        }
+        static string GetRecognitionResultReason(SpeechRecognitionResult result) =>
+            result.Reason switch
+            {
+                ResultReason.RecognizedSpeech => $"Recorgnized:\"{result.Text}\"",
+                ResultReason.NoMatch => "NOMATCH",
+                ResultReason.Canceled => GetCancellationResult(result),
+                _ => $"Unhandled {result.Reason}"
+            };
+        static string GetCancellationResult(SpeechRecognitionResult result)
+        {
+            var cancellation = CancellationDetails.FromResult(result);
+            var reason = $"Canceled {cancellation.Reason}";
+            if (cancellation.Reason == CancellationReason.Error)
+            {
+                reason += $"Canceled{cancellation.ErrorCode} Details {cancellation.ErrorDetails}";
+            }
+            MessageBox.Show(reason);
+            return reason;
+        }
+    }
+}
